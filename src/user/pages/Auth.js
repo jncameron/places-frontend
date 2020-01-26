dfc0965cda340c48fcd23be4ef773c1c1b4318ac
John Cameron <john.n.cameron@gmail.com>
2020-01-26 12:31:39 +1100

    image uploader

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -13,6 +13,7 @@ import {
 import { useForm } from "../../shared/hooks/form-hook";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 import { AuthContext } from "../../shared/context/auth-context";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import "./Auth.css";
 
 const Auth = () => {
@@ -39,7 +40,8 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
+          image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -50,8 +52,13 @@ const Auth = () => {
           name: {
             value: "",
             isValid: false
+          },
+          image: {
+            value: null,
+            isValid: false
           }
         },
+
         false
       );
     }
@@ -60,6 +67,8 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+
+    console.log(formState.inputs);
 
     if (isLoginMode) {
       try {
@@ -114,6 +123,9 @@ const Auth = () => {
               errorText="Please enter a name."
               onInput={inputHandler}
             />
+          )}
+          {!isLoginMode && (
+            <ImageUpload center id="image" onInput={inputHandler} />
           )}
           <Input
             element="input"
